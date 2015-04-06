8b94493a86226d616e917ecab96c59274850bb70
Antriksh <antriksh@antriksh.(none)>
2015-04-06 21:01:31 +0530

    still not working properly !

--- Divide And Conquer/mergesort.cpp
+++ Divide And Conquer/mergesort.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
 #include <math.h>
+#define N 5
 using namespace std;
 int *a = new int[10];
 
 int merge(int p, int q, int r){
-	int n1 = q-p+r;
-	int n2 = r-q;
+	cout<<p<<q<<r;
+	int n1 = q-p+r; //3
+	int n2 = r-q;	//1
 	int i,j;
-	int *l = new int[10];
-	int *k = new int[10];
-	for(i=0;i<n1;i++){
-		l[i] = a[p+i-1];
+	int l[10];
+	int k[10];
+	for(i=1;i<=n1;i++){
+		l[i-1] = a[p+i-1]; // a[0], a[1], a[2]
 	}
-	for(i=0;i<n2;i++){
-		k[i] = a[q+i];
+	for(i=1;i<=n2;i++){
+		k[i-1] = a[q+i]; // a[0]
 	}
-	l[n1+1]=NULL;
-	k[n2+1]=NULL;
+	l[n1]='\0';
+	k[n2]='\0';
 	j=0;
 	i=0;
-	for(int n=p; n<=r;n++){
-		cout<<".";
-		if(l[i] <= k[j] && l[i]!=NULL && k[j]!=NULL) {
+	for(int n=p;n<r;n++){ // 0 to 2
+		cout<<"|";
+		if(l[i] <= k[j] && l[i]!='\0' && k[j]!='\0') {
 			a[n] = l[i];
+			a[n+1] = k[j];
 			i++;
 		}
-		else if(l[i] >= k[j] && l[i]!=NULL && k[j]!=NULL){
+		else if(l[i] >= k[j] && l[i]!='\0' && k[j]!='\0'){
 			a[n] = k[j];
+			a[n+1] = l[i];
 			j++;
+		}
+		for(int i=0;i<N;i++){
+			cout<<a[i];
 		}
 	}
 }
@@ -46,15 +53,16 @@ int main(){
 	
 	cout<<"\t\t:-: MERGE SORT :-: "<<endl;
 
-	cout<<"Enter 10 numbers : "<<endl;
-	for(int i=0;i<10;i++){
+	cout<<"Enter 5 numbers : "<<endl;
+	for(int i=0;i<N;i++){
 		cin>>a[i];
 	}
 	cout<<"Sorting .";
 
-	mergesort(0,10);
-	for(int i=0;i<10;i++){
+	mergesort(0,N);
+
+	for(int i=0;i<N;i++){
 		cout<<endl<<a[i];
 	}
-	cout<<"\t\t-- END --\n\n";
+	cout<<"\n\t\t-- END --\n\n";
 }
