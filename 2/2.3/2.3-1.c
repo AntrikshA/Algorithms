d08dc55f855c62a74d354e0e65abc79c8551b193
Antriksh <thechef@mymail.antriksh.com>
2015-05-31 16:00:20 +0530

    2.2 and 2.3 added !

---
+++ 2/2.3/2.3-1.c
@@ -0,0 +1,11 @@
+/* Using Figure 2.4 as a model, illustrate the operation of merge sort on the array
+A = {3; 41; 52; 26; 38; 57; 9; 49}.
+*/
+
+#include <stdio.h>
+
+int main(){
+	int i, j;
+	int A[] = {3, 41, 52, 26, 38, 57, 9, 49};
+	
+}
