9d8b004b44d464345c3db32c5f09ee95bc1f38e3
evamy <antriksh5235@gmail.com>
2015-11-07 18:42:28 +0530

    Codesprint codes + PageRank(I guess done)

--- MMDS/PageRank/PageRank.py
+++ MMDS/PageRank/PageRank.py
@@ -3,49 +3,60 @@ import numpy as np
 from decimal import *
 import pandas as pd
 from pandas import Categorical
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, csgraph, csc_matrix
 
-size = 875713
 
 def PageRank():
 
     print "extracting file data..."
     start = time.time()
-    webGoogle = pd.read_csv('web-Google.txt', sep='\t', comment='#', names=['from', 'to'],\
-         )
+    webGoogle = pd.read_csv('web-Google.txt', sep='\t', comment='#', names=['from', 'to'])
     end = time.time()
     print "Time to read the file :", end - start
 
-    print pd.Categorical(webGoogle['from']).Length
-    # print webGoogle
+    print "initialising matrices..."
+    start = time.time()
+    data = np.repeat(1, webGoogle.shape[0])
+    categories = np.unique(webGoogle.values)
+    size = len(categories)
 
-    # while webGoogle:
-    #     print "initialising matrices..."
-    #     start = time.time()
-    #     data = np.repeat(1, webGoogle.shape[0])
-    #     categories = np.unique(webGoogle.values) # finds all unique elements in array
+    col_ind = pd.Categorical(webGoogle['from'], categories=categories).codes
+    row_ind = pd.Categorical(webGoogle['to'], categories=categories).codes
 
-    #     col_ind = pd.Categorical(webGoogle['from'])
-    #     row_ind = pd.Categorical(webGoogle['to'])
-        
-    #     M = M.multiply(csr_matrix(1 / M.sum(axis=0)))
+    M = csr_matrix((data, (row_ind, col_ind)))
 
-    #     # M.todense()
-    #     end = time.time()
-    #     print "Time to initialise matrices :", end - start
+    end = time.time()
+    print "Time to initialise matrices :", end - start
 
-    #     rank = np.full((size+1,), fill_value=(Decimal(1/Decimal(size))), dtype=np.float)
+    start = time.time()
+    beta = 0.85
+    rank = np.ones(size)
+    t = 1
+    while 1:
+        rank0 = rank.copy()
+        rankp = np.zeros(size)
+        for i in categories:
+            try:
+                for j in range(i):
+                    rankp[i] +=  float(beta * float(float(rank[j]) / float(M[i].getnnz())))
+            except ZeroDivisionError:
+                rankp[i] +=  0
+                continue
 
-    #     start = time.time()
-    #     while 1:
-    #         prevrank = rank
-    #         rank = M.dot(rank)
-    #         if rank.all() == prevrank.all():
-    #             break
+        S = float(sum(rankp))
 
-    #     end = time.time()
-    #     print "Time to calculate ranks :", end-start
-    #     print rank
+        for i in categories:
+            rank[i] = float(float(rankp[i]) + float((1-S)/float(size)))
+
+        t += 1
+        if rank.all() == rank0.all():
+            break
+
+    end = time.time()
+    print "Time to calculate ranks :", end - start
+    print "Iterations :",t
+    print float(rank[99]/float(sum(rank)))
+
 
 if __name__ == '__main__':
     PageRank()
