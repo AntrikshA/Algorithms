478674850dbbf043ac7ec4b9fcbe03ea69b8736e
evamy <antriksh5235@gmail.com>
2016-01-03 23:10:07 +0530

    Minimum Cut algo not done yet

---
+++ ADA/MinCut.cpp
@@ -0,0 +1,14 @@
+// Minimum Cut Problem
+
+#include "iostream"
+#include "fstream"
+#include "sstream"
+#include "cstring"
+#include "cstdlib"
+
+using namespace std;
+
+#define INPUT_FILE "kargerMinCut.txt"
+
+int SIZE, array[10000];
+long int comparisons=0;
