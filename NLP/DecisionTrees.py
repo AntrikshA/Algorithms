ee09964ae6e213d12796e3f13571c19c4793eb45
evamy <antriksh5235@gmail.com>
2016-01-21 14:20:47 +0530

    Some tests and scripts for learning

---
+++ NLP/DecisionTrees.py
@@ -0,0 +1,20 @@
+#Decision Trees to decide end of sentence
+
+class DecisionTrees():
+	# Main class
+	infogain
+
+	def __init__(self):
+		print 'Welcome to Decision Trees'
+
+	def train(self):
+		print 'Training Decision Trees'
+
+	def test(self):
+		self.train()
+		print 'Testing Decision Trees'
+		print 'Checking for accuracy'
+
+	def classify(self):
+		print 'Taking the input and classifying'
+
